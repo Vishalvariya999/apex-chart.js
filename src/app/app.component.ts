69a032e6ce236f223490a78366c8d75cf3747939
Vishalvariya999 <110755112+Vishalvariya999@users.noreply.github.com>
2023-07-08 09:20:35 +0530

    Changes in dashboard routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'statistics';
+
+  public routerData: any = [
+    {
+      lable: 'Apex chart',
+      link: '',
+    },
+    {
+      lable: 'Chart.js',
+      link: '/dashboard-chartjs',
+    },
+  ];
 }
